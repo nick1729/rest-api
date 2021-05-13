109052f831a53181f3ee04ef95daaf41b5000ffa
Nick <nick1729_github@mail.ru>
2021-05-13 14:26:19 +0300

    6

--- main.go
+++ main.go
@@ -40,6 +40,8 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Print("The database is connected")
 }
 
 // Show start page
@@ -51,11 +53,6 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 // Add new user
 // http://127.0.0.1:8000/users?Firstname=Gena&Lastname=Ivanov&Email=gena@mail.gg&Age=44
 func addUser(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "POST" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
 
 	var (
 		user tUser
@@ -87,16 +84,13 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 		user.Firstname, user.Lastname, user.Email, user.Age, time.Now()).Scan(&user.ID)
 
 	msg = fmt.Sprintf("The user %v was added", user.ID)
+	log.Print(msg)
 	fmt.Fprint(w, msg)
 }
 
 // Show user by ID
+// http://127.0.0.1:8000/users/a5657a25-b62d-45f8-96f6-41aab04f9ec0
 func showUser(w http.ResponseWriter, r *http.Request) {
-
-	if r.Method != "GET" {
-		http.Error(w, http.StatusText(405), 405)
-		return
-	}
 
 	vars := mux.Vars(r)
 	id := vars["key"]
@@ -129,16 +123,65 @@ func showUser(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, msg)
 }
 
+// Edit user data by ID
+func editUser(w http.ResponseWriter, r *http.Request) {
+
+	var (
+		user    tUser
+		err     error
+		id, msg string
+	)
+
+	id = r.FormValue("ID")
+	user.Firstname = r.FormValue("Firstname")
+	user.Lastname = r.FormValue("Lastname")
+	user.Email = r.FormValue("Email")
+	user.Age, err = strconv.ParseUint(r.FormValue("Age"), 10, 32)
+
+	switch {
+	case isUUID(id) != true:
+		http.Error(w, "Incorrect users ID!", http.StatusBadRequest)
+		return
+	case user.Firstname == "" || len(user.Firstname) > 256:
+		http.Error(w, "Incorrect users firstname!", http.StatusBadRequest)
+		return
+	case user.Lastname == "" || len(user.Lastname) > 256:
+		http.Error(w, "Incorrect users lastname!", http.StatusBadRequest)
+		return
+	case user.Email == "" || len(user.Email) > 256 || isEmail(user.Email) != true:
+		http.Error(w, "Incorrect users email!", http.StatusBadRequest)
+		return
+	case err != nil || user.Age < 1 || user.Age > 256:
+		http.Error(w, "Incorrect users age!", http.StatusBadRequest)
+		return
+	}
+
+	_, err = db.Exec("UPDATE users SET firstname = $1, lastname = $2, email = $3, age = $4, created = $5 WHERE id = $6",
+		user.Firstname, user.Lastname, user.Email, user.Age, time.Now(), id)
+	if err != nil {
+		msg = fmt.Sprintf("Failed to update user %s\n", id)
+		log.Print(msg)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	msg = "Users data successfully updated"
+	log.Print(msg)
+	fmt.Fprintln(w, msg)
+}
+
 func main() {
 
-	//Routing
+	// Routing
 	router := mux.NewRouter()
-	router.HandleFunc("/users/{key}", showUser)
-	router.HandleFunc("/users", addUser)
+	router.HandleFunc("/users/", editUser).Methods("PUT")
+	router.HandleFunc("/users/{key}", showUser).Methods("GET")
+	router.HandleFunc("/users", addUser).Methods("POST")
 	router.HandleFunc("/", homePage)
 	http.Handle("/", router)
 
-	fmt.Println("Server is listening...")
+	// Start service
+	log.Print("Server is listening...")
 	if err := http.ListenAndServe(":8000", nil); err != nil {
 		log.Fatal(err)
 	}

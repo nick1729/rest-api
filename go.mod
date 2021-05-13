34bcda720e5ba4790af3be503bc1e3f875a179c6
Nick <nick1729_github@mail.ru>
2021-05-13 10:26:00 +0300

    2

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module rest-api
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.1
+)
--- main.go
+++ main.go
@@ -1,9 +1,62 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
+const (
+	dUser = "login"
+	dPass = "pass"
+	dHost = "url"
+	dPort = "port"
+	dName = "table"
+)
+
+var db *sql.DB
+
+// Open DB and check connection
+func init() {
+
+	var (
+		connStr         string
+		errCon, errPing error
+	)
+
+	connStr = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dHost, dPort, dUser, dPass, dName)
+
+	db, errCon = sql.Open("postgres", connStr)
+	if errCon != nil {
+		log.Fatal(errCon)
+	}
+
+	errPing = db.Ping()
+	if errPing != nil {
+		log.Fatal(errPing)
+	}
+}
+
+//Show start page
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "Its start page! If u can read this db works correctly =)")
+}
+
 func main() {
-	fmt.Println("Hello World!")
+
+	//Routing
+	router := mux.NewRouter()
+	router.HandleFunc("/", homePage)
+	http.Handle("/", router)
+
+	fmt.Println("Server is listening...")
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		log.Fatal(err)
+	}
 }

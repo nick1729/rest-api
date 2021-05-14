4afb55529262408d203f4d4130601135bc6acf33
Nick <nick1729_github@mail.ru>
2021-05-14 06:41:15 +0300

    8

--- handlers.go
+++ handlers.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -26,7 +27,7 @@ func isEmail(e string) bool {
 	return true
 }
 
-// Email validation
+// UUID validation
 func isUUID(u string) bool {
 
 	var err error
@@ -40,50 +41,36 @@ func isUUID(u string) bool {
 }
 
 // Add new user
-// http://127.0.0.1:8000/users?Firstname=Gena&Lastname=Ivanov&Email=gena@mail.gg&Age=44
+// /users?firstname=Gena&lastname=Ivanov&email=qweqw@mail.gg&age=29
 func addUser(w http.ResponseWriter, r *http.Request) {
 
 	var (
-		user tUser
-		err  error
-		msg  string
+		u   tUser
+		err error
+		msg string
 	)
 
-	// read keys
-	user.Firstname = r.FormValue("Firstname")
-	user.Lastname = r.FormValue("Lastname")
-	user.Email = r.FormValue("Email")
-	user.Age, err = strconv.ParseUint(r.FormValue("Age"), 10, 32)
-
-	// check data
-	switch {
-	case user.Firstname == "" || len(user.Firstname) > 256:
-		http.Error(w, "Incorrect users firstname!", http.StatusBadRequest)
-		return
-	case user.Lastname == "" || len(user.Lastname) > 256:
-		http.Error(w, "Incorrect users lastname!", http.StatusBadRequest)
-		return
-	case user.Email == "" || len(user.Email) > 256 || isEmail(user.Email) != true:
-		http.Error(w, "Incorrect users email!", http.StatusBadRequest)
-		return
-	case err != nil || user.Age < 1 || user.Age > 256:
-		http.Error(w, "Incorrect users age!", http.StatusBadRequest)
+	u, err = checkKeys(r)
+	if err != nil {
+		msg = fmt.Sprintf("Error! %s", err.Error())
+		http.Error(w, msg, http.StatusBadRequest)
 		return
 	}
 
 	// insert new data into db
 	db.QueryRow("INSERT INTO Users (firstname, lastname, email, age, created) VALUES ($1, $2, $3, $4, $5) RETURNING id",
-		user.Firstname, user.Lastname, user.Email, user.Age, time.Now()).Scan(&user.ID)
+		u.Firstname, u.Lastname, u.Email, u.Age, time.Now()).Scan(&u.ID)
 
-	msg = fmt.Sprintf("The user %v was added", user.ID)
+	msg = fmt.Sprintf("The user %v was added", u.ID)
 	fmt.Fprint(w, msg)
 	log.Print(msg)
 }
 
 // Show user by ID
-// http://127.0.0.1:8000/users/a5657a25-b62d-45f8-96f6-41aab04f9ec0
+// /users/a5657a25-b62d-45f8-96f6-41aab04f9ec0
 func showUser(w http.ResponseWriter, r *http.Request) {
 
+	// read key
 	vars := mux.Vars(r)
 	id := vars["key"]
 
@@ -97,11 +84,11 @@ func showUser(w http.ResponseWriter, r *http.Request) {
 	// select table row
 	row := db.QueryRow("SELECT * FROM users WHERE id = $1", id)
 
-	user := tUser{}
+	u := tUser{}
 
 	// scan data
-	err := row.Scan(&user.ID, &user.Firstname, &user.Lastname,
-		&user.Email, &user.Age, &user.Created)
+	err := row.Scan(&u.ID, &u.Firstname, &u.Lastname,
+		&u.Email, &u.Age, &u.Created)
 
 	// handle error
 	switch err {
@@ -117,49 +104,36 @@ func showUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("User data:\n%v, %s, %s, %d, %v\n", user.ID,
-		user.Firstname, user.Lastname, user.Age, user.Created)
+	msg := fmt.Sprintf("User data:\n%v, %s, %s, %d, %v\n", u.ID,
+		u.Firstname, u.Lastname, u.Age, u.Created)
 	fmt.Fprint(w, msg)
 }
 
 // Edit user data by ID
+// /users/?id=a5657a25-b62d-45f8-96f6-41aab04f9ec0&firstname=Qwe&lastname=Rty&email=qwe@rty.gg&age=23
 func editUser(w http.ResponseWriter, r *http.Request) {
 
 	var (
-		user    tUser
+		u       tUser
 		err     error
 		id, msg string
 	)
 
-	// read keys
-	id = r.FormValue("ID")
-	user.Firstname = r.FormValue("Firstname")
-	user.Lastname = r.FormValue("Lastname")
-	user.Email = r.FormValue("Email")
-	user.Age, err = strconv.ParseUint(r.FormValue("Age"), 10, 32)
+	id = r.FormValue("id")
+	if isUUID(id) != true {
+		http.Error(w, "Error! Incorrect user ID!", http.StatusBadRequest)
+	}
 
-	// check data
-	switch {
-	case isUUID(id) != true:
-		http.Error(w, "Incorrect users ID!", http.StatusBadRequest)
-		return
-	case user.Firstname == "" || len(user.Firstname) > 256:
-		http.Error(w, "Incorrect users firstname!", http.StatusBadRequest)
-		return
-	case user.Lastname == "" || len(user.Lastname) > 256:
-		http.Error(w, "Incorrect users lastname!", http.StatusBadRequest)
-		return
-	case user.Email == "" || len(user.Email) > 256 || isEmail(user.Email) != true:
-		http.Error(w, "Incorrect users email!", http.StatusBadRequest)
-		return
-	case err != nil || user.Age < 1 || user.Age > 256:
-		http.Error(w, "Incorrect users age!", http.StatusBadRequest)
+	u, err = checkKeys(r)
+	if err != nil {
+		msg = fmt.Sprintf("Error! %s", err.Error())
+		http.Error(w, msg, http.StatusBadRequest)
 		return
 	}
 
 	// update row data
 	_, err = db.Exec("UPDATE users SET firstname = $1, lastname = $2, email = $3, age = $4, created = $5 WHERE id = $6",
-		user.Firstname, user.Lastname, user.Email, user.Age, time.Now(), id)
+		u.Firstname, u.Lastname, u.Email, u.Age, time.Now(), id)
 	if err != nil {
 		msg = fmt.Sprintf("Failed to update user %s\n", id)
 		http.Error(w, msg, http.StatusBadRequest)
@@ -170,4 +144,37 @@ func editUser(w http.ResponseWriter, r *http.Request) {
 	msg = "Users data successfully updated"
 	fmt.Fprintln(w, msg)
 	log.Print(msg)
+}
+
+//
+func checkKeys(r *http.Request) (tUser, error) {
+
+	var (
+		u   tUser
+		err error
+	)
+
+	// read keys
+	u.Firstname = r.FormValue("firstname")
+	u.Lastname = r.FormValue("lastname")
+	u.Email = r.FormValue("email")
+	u.Age, err = strconv.ParseUint(r.FormValue("age"), 10, 32)
+
+	// check data
+	switch {
+	case u.Firstname == "" || len(u.Firstname) > 256:
+		err = errors.New("Incorrect user firstname!")
+		return u, err
+	case u.Lastname == "" || len(u.Lastname) > 256:
+		err = errors.New("Incorrect user lastname!")
+		return u, err
+	case u.Email == "" || len(u.Email) > 256 || isEmail(u.Email) != true:
+		err = errors.New("Incorrect user email!")
+		return u, err
+	case err != nil || u.Age < 1 || u.Age > 256:
+		err = errors.New("Incorrect user age!")
+		return u, err
+	}
+
+	return u, nil
 }
--- handlers_test.go
+++ handlers_test.go
@@ -8,44 +8,44 @@ import (
 func TestIsEmail(t *testing.T) {
 
 	var (
-		vGot, vExpd bool
-		e           string
+		got, expd bool
+		e         string
 	)
 
 	// testing correct email #1
 	e = "qwerty@gmail.com"
-	vExpd = true
+	expd = true
 
-	vGot = isEmail(e)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isEmail(e)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing correct email #2
 	e = "qwer234@mail.ru"
-	vExpd = true
+	expd = true
 
-	vGot = isEmail(e)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isEmail(e)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing incorrect email #1
 	e = "qweriopmail.ru"
-	vExpd = false
+	expd = false
 
-	vGot = isEmail(e)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isEmail(e)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing incorrect email #2
 	e = "qwerty@proton,com"
-	vExpd = false
+	expd = false
 
-	vGot = isEmail(e)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isEmail(e)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 }
 
@@ -53,43 +53,43 @@ func TestIsEmail(t *testing.T) {
 func TestIsUUID(t *testing.T) {
 
 	var (
-		vGot, vExpd bool
-		u           string
+		got, expd bool
+		u         string
 	)
 
 	// testing correct UUID #1
 	u = "a5657a25-b62d-45f8-96f6-41aab04f9ec0"
-	vExpd = true
+	expd = true
 
-	vGot = isUUID(u)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isUUID(u)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing correct UUID #2
 	u = "c30946e2-eb83-45fb-99e1-44e5ea1528ac"
-	vExpd = true
+	expd = true
 
-	vGot = isUUID(u)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isUUID(u)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing incorrect UUID #1
 	u = "qweqw-342423-mttyu-45"
-	vExpd = false
+	expd = false
 
-	vGot = isUUID(u)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isUUID(u)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 
 	// testing incorrect UUID #2
 	u = "76.34%ter&das32HEWfew"
-	vExpd = false
+	expd = false
 
-	vGot = isUUID(u)
-	if vGot != vExpd {
-		t.Error("Expected:", vExpd, "got:", vGot)
+	got = isUUID(u)
+	if got != expd {
+		t.Error("Expected:", expd, "got:", got)
 	}
 }
